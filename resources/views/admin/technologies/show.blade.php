cf990af49173909dc8c17ea628e5528c6d7e60b3
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-01-18 09:30:48 +0100

    updated

--- resources/views/admin/technologies/show.blade.php
+++ resources/views/admin/technologies/show.blade.php
@@ -6,11 +6,11 @@
             @foreach ($technology->projects as $project)
                 <li class="list-unstyled border-bottom pb-2 d-flex align-items-center ">
                     <div id="circle-image">
-                        <a href="{{ route('admin.technologies.show', $project->slug) }}">
+                        <a href="{{ route('admin.projects.show', $project->slug) }}">
                             <img src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->title }}">
                         </a>
                     </div>
-                    <a href="{{ route('admin.technologies.show', $project->slug) }}" class="mx-3">{{ $project->title }}</a>
+                    <a href="{{ route('admin.projects.show', $project->slug) }}" class="mx-3">{{ $project->title }}</a>
                 </li>
             @endforeach
         </ul>
